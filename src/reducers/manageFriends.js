8584fc7b08096bb939a298867ac980d5a50ac3e1
fenglisa <lisafeng10@gmail.com>
2020-10-29 09:51:40 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,29 @@
+// let state = {
+//   friends: []
+// }
+//
+// const actions = {
+//   addFriend: {
+//     type: "ADD_FRIEND",
+//     friend: {
+//         name: "Chrome Boi"
+//         homewtown: "NYC",
+//         id: 1
+//       }
+//   },
+//   removeFriend: {
+//     type: "REMOVE_FRIEND",
+//     id: 1
+//   }
+// }
+
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {friends: state.friends.concat(action.friend)}
+    case 'REMOVE_FRIEND':
+      return {friends: state.friends.filter(friend => friend.id != action.id)}
+    default:
+      return state;
+  }
 }
