623f17d5f5c13f10244099e4776cb6781cdf63bd
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-11-19 13:32:43 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return ({
+                friends: [
+
+                    ...state.friends, 
+                    {
+                        name: "Joe",
+                        hometown: "Boston",
+                        id: 101
+                    }
+                ]
+            })    
+        case 'REMOVE_FRIEND':
+            // create new variable with value of object that needs to be deleted 
+            let deleteIt = state.friends.find(element => element.id === action.id)
+            return ({
+                //Use filter() method  to output a copy of state without the object that we want deleted
+                friends: state.friends.filter(element => {
+                    return element.id != deleteIt.id
+                })
+            })
+        default:
+            return state;
+    }
 }
